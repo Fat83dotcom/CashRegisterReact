e3cd4b5cbb3309bb1030b1c7dd5eccddd0b5273b
fat83dotcom <fernando.quimic@gmail.com>
2026-03-04 21:08:24 -0300

    Implementando o AppShell  do Mantine

--- CashRegisterApp/src/App.tsx
+++ CashRegisterApp/src/App.tsx
@@ -3,6 +3,8 @@ import "./styles/global.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./styles/theme";
 import { CollapseDesktop } from "./pages/Main";
+import "@mantine/core/styles.css";
+import "./styles/global.css";
 
 export function App() {
   return (
--- CashRegisterApp/src/pages/Main/index.tsx
+++ CashRegisterApp/src/pages/Main/index.tsx
@@ -1,4 +1,4 @@
-import { AppShell, Button } from "@mantine/core";
+import { AppShell, Burger, Group, Text } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 export function CollapseDesktop() {
@@ -15,15 +15,32 @@ export function CollapseDesktop() {
         collapsed: { mobile: !mobileOpened, desktop: !desktopOpened },
       }}
     >
-      <AppShell.Header>Header</AppShell.Header>
-      <AppShell.Navbar>Navbar</AppShell.Navbar>
+      <AppShell.Header>
+        <Group h="100%" px="md">
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
+        </Group>
+        <Group></Group>
+      </AppShell.Header>
+      <AppShell.Navbar p="md">
+        You can collapse the Navbar both on desktop and mobile. After sm
+        breakpoint, the navbar is no longer offset by padding in the main
+        element and it takes the full width of the screen when opened.
+      </AppShell.Navbar>
       <AppShell.Main>
-        <Button onClick={toggleDesktop} visibleFrom="sm">
-          Toggle navbar
-        </Button>
-        <Button onClick={toggleMobile} hiddenFrom="sm">
-          Toggle navbar
-        </Button>
+        <Text>This is the main section, your app content here.</Text>
+        <Text>The navbar is collapsible both on mobile and desktop. Nice!</Text>
+        <Text>Mobile and desktop opened state can be managed separately.</Text>
       </AppShell.Main>
     </AppShell>
   );
