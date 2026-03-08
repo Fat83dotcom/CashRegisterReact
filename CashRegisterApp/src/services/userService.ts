219f3cf26500caa4a7064ab2c4e0a9b9d06f540d
fat83dotcom <fernando.quimic@gmail.com>
2026-03-07 21:06:04 -0300

    Mostrando uma mensagem de sucesso ao salvar um usuario

--- CashRegisterApp/src/services/userService.ts
+++ CashRegisterApp/src/services/userService.ts
@@ -1,9 +1,11 @@
-// src/services/userService.ts
+import { IconCheck } from "@tabler/icons-react";
 import { apiClient } from "../api/api";
 import type { ICreateUserResponse } from "../pages/User/Interfaces/ICreateUserResponse";
 // Importamos a interface que criamos anteriormente
 import type { ICreateUserRequest } from "../pages/User/Interfaces/ICreateUserRequest";
 import type { IGetAllUsersResponse } from "../pages/User/Interfaces/IGetAllUsersResponse";
+import { notifications } from "@mantine/notifications";
+import React from "react";
 
 export const UserService = {
   getAll: () => apiClient.get<IGetAllUsersResponse[]>("/user"),
@@ -11,7 +13,19 @@ export const UserService = {
   getById: (id: number) => apiClient.get<IGetAllUsersResponse>(`/user/${id}`),
 
   create: (data: ICreateUserRequest) =>
-    apiClient.post<ICreateUserResponse, typeof data>("/user", data),
+    apiClient
+      .post<ICreateUserResponse, typeof data>("/user", data)
+      .then((response) => {
+        if (response.id > 0) {
+          notifications.show({
+            title: "Sucesso",
+            message: "Usuário criado com sucesso.",
+            color: "green",
+            autoClose: 5000,
+            icon: React.createElement(IconCheck),
+          });
+        }
+      }),
 
   delete: (id: number) => apiClient.delete<void>(`/user/${id}`),
 };
