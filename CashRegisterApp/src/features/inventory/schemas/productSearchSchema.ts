c408eef4806952720d7ecf3e7021a1a94a12f4ef
fat83dotcom <fernando.quimic@gmail.com>
2026-05-31 12:37:29 -0300

    Introduzindo preproccess para este caso onde é recebido um id

--- CashRegisterApp/src/features/inventory/schemas/productSearchSchema.ts
+++ CashRegisterApp/src/features/inventory/schemas/productSearchSchema.ts
@@ -2,7 +2,11 @@ import { z } from "zod";
 
 export const productSearchSchema = z.object({
   searchTerm: z.string().optional(),
-  categoryId: z.string().optional(),
+  categoryId: z
+    .preprocess((value) => {
+      return value === null || value === undefined ? "0" : value;
+    }, z.string())
+    .optional(),
 });
 
 export type ProductSearchFormData = z.infer<typeof productSearchSchema>;
